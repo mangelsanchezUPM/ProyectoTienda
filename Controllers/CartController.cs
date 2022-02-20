83495a64746e625d6c46ceb04f18bb497b1e09b4
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2022-02-20 21:45:28 +0100

    Listado de Productos y añadir al Carrito

---
+++ Controllers/CartController.cs
@@ -0,0 +1,40 @@
+﻿using ProyectoTienda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTienda.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Cart/Add/{id}
+        [Authorize]
+        public ActionResult Add(Cart cart, int id)
+        {
+            Model1Container db = new Model1Container();
+            Product product = db.Products.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            
+            cart.Add(product);
+            return RedirectToAction("Index", "Products");
+        }
+
+        // GET: Cart/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
